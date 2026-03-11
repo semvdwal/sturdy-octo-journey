88eea7b55c2d1df1890d6dcd0cf49d660076420c
wal0s11 <wal_s_van_der@belastingdienst.nl>
2026-03-11 12:02:00 +0100

    Minimilized producer, determined actual cause

--- src/main/java/org/acme/reconciler/SampleReconciler.java
+++ src/main/java/org/acme/reconciler/SampleReconciler.java
@@ -3,17 +3,15 @@ package org.acme.reconciler;
 
 import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
-import io.quarkiverse.operatorsdk.annotations.CSVMetadata;
-import io.strimzi.api.kafka.model.topic.KafkaTopic;
 import org.acme.resource.Sample;
 
 @Workflow(
-    dependents = {
-        @Dependent(type = KafkaTopicDependentResource.class)
-    }
+        dependents = {
+                @Dependent(type = KafkaTopicDependentResource.class)
+        }
 )
 @ControllerConfiguration()
-public class SecondReconciler implements Reconciler<Sample> {
+public class SampleReconciler implements Reconciler<Sample> {
 
     @Override
     public UpdateControl<Sample> reconcile(Sample resource, Context<Sample> context) {
